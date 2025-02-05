21cda210db84cd71f010345322420b84c48a8ce3
Jared Ju <jaredju24@gmail.com>
2025-02-05 15:24:26 -0800

    Removed the print statement from the SCsub.py file

--- modules/ai_rest/SCsub.py
+++ modules/ai_rest/SCsub.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+'''#!/usr/bin/env python
 from misc.utility.scons_hints import *
 
 Import("env")
@@ -7,4 +7,4 @@ Import("env_modules")
 env_ai_rest = env_modules.Clone()
 
 # Add all the C++ source files in this module
-env_ai_rest.add_source_files(env.modules_sources, "*.cpp")
+env_ai_rest.add_source_files(env.modules_sources, "*.cpp")'''
